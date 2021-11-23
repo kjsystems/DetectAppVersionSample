3e8584a6504ccefd9e01a83f1b3a04d71226cd72
Satoru Fujimori <satoru.fujimori@gmail.com>
2021-11-23 21:45:23 +0900

    Function側で最新バージョンを返すように修正

--- FunctionApp/Function1.cs
+++ FunctionApp/Function1.cs
@@ -10,26 +10,21 @@ using System.Threading.Tasks;
 
 namespace FunctionApp
 {
+    public class AppInfo
+    {
+        public string Version => "2.0.0";
+    }
+
     public static class Function1
     {
-        [FunctionName("Function1")]
+
+
+        [FunctionName("GetVersion")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(new AppInfo());
         }
     }
 }
